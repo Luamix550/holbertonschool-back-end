d3ca1b31f4c9986e2d6a80182e12c6ee1d1094ca
Luamix <luizalfonso611@gmail.com>
2024-05-01 16:01:30 -0500

    task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import requests
+import csv
+from sys import argv
+
+if __name__ == "__main__":
+
+    id = int(argv[1])
+
+    emp_r = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    all_data = emp_r.json()
+    username = all_data.get("username")
+
+    t = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={id}')
+    tasks = t.json()
+
+    filename = f"{id}.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"')
+        for task in tasks:
+            completed_status = "True" if task["completed"] else "False"
+            writer.writerow([id, username, completed_status, task["title"]])
